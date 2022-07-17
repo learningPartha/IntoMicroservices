9893bd9108b02da18bfd9aa8da54a13648dfc34b
learningPartha <pchowdhury.apple@gmail.com>
2022-07-17 21:08:05 +0530

    Eureka service
    Register with discovery server

---
+++ discovery-server/src/test/java/com/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.discoveryserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- movie-catalog-service/src/main/java/com/moviecatalog/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/moviecatalog/resource/MovieCatalogResource.java
@@ -1,10 +1,11 @@
 package com.moviecatalog.resource;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.moviecatalog.model.CatalogItem;
 import com.moviecatalog.model.Movie;
-import com.moviecatalog.model.Rating;
+//import com.moviecatalog.model.Rating;
 import com.moviecatalog.model.UserRating;
 
 @RestController
@@ -27,13 +28,17 @@ public class MovieCatalogResource {
 	 * @Autowired private WebClient.Builder webClientBuilder;
 	 */
 
+	/*
+	 * @Autowired private DiscoveryClient discoveryClient;
+	 */
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
 		// get all movie id
 		//List<Rating> ratings = Arrays.asList(new Rating("rowl", 5), new Rating("tolk", 9));
 		
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingsdata/users/" + userId, UserRating.class);
 			
 
 		return ratings.getUserRatings().stream().map(rating -> {
@@ -42,7 +47,7 @@ public class MovieCatalogResource {
 			// Movie.class);
 			// return new CatalogItem(movie.getMovieName(), "Epic Fantasy",
 			// rating.getRating());
-			Movie[] movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(),
+			Movie[] movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(),
 					Movie[].class);
 
 			/*
