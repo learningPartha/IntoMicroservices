287d2b23a0e422302b9e15a1ef251bffb27cab0e
learningPartha <51481182+learningPartha@users.noreply.github.com>
2022-07-25 19:54:00 +0530

    Spring Security
    Basic, JDBC and JPA

---
+++ spring-security-basic/src/main/java/com/springsecurity/control/HomeResource.java
@@ -0,0 +1,27 @@
+package com.springsecurity.control;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	@GetMapping("/")
+	public String getHome() {
+		return "<h1>Welcome</h1>";
+		
+	}
+
+	@GetMapping("/admin")
+	public String getHomeAdmin() {
+		return "<h1>Welcome Admin</h1>";
+
+	}
+
+	@GetMapping("/user")
+	public String getHomeUser() {
+		return "<h1>Welcome User</h1>";
+
+	}
+	
+}
